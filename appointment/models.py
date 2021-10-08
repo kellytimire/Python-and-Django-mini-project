088475e888cc4427171fc6de70901fbb0d452779
KartikTakyar0046 <42770126+KartikTakyar0046@users.noreply.github.com>
2021-10-08 12:32:45 +0530

    created a new profile class instead of auth django

--- appointment/models.py
+++ appointment/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import Permission, User
 
+
+
+class Profile(models.Model):
+    user=models.ForeignKey(User,default=1)
+    aadhar_no=models.CharField(max_length=16)
+    def __str__(self):
+        return self.user.username
+
 # Create your models here.
 class Appointment(models.Model):
     user = models.ForeignKey(User, default=1)
